539587ded1d69869aea893b3d494bfad9f53c6c0
Sushant Agrawal <capitalch@gmail.com>
2022-08-26 21:13:49 +0530

    init

--- lab/KaterServer/graphql_container/graphql_artifacts.py
+++ lab/KaterServer/graphql_container/graphql_artifacts.py
@@ -1,41 +1,61 @@
-from ariadne import ObjectType, QueryType, gql, make_executable_schema
+from ariadne import graphql_sync, ObjectType, QueryType, gql, make_executable_schema, load_schema_from_path
 from ariadne.asgi import GraphQL
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from ariadne.constants import PLAYGROUND_HTML
 
-graphQlArtifacts = Blueprint('graphQlArtifacts',__name__)
+graphQlArtifacts = Blueprint('graphQlArtifacts', __name__)
 
 
-@graphQlArtifacts.route('/graphql',methods=['GET'])
+@graphQlArtifacts.route('/graphql', methods=['GET'])
 def graphql_playground():
-     return PLAYGROUND_HTML, 200
+    return PLAYGROUND_HTML, 200
 
-type_defs = gql("""
-    type Query {
-        people:[Person]!
-    }
 
-    type Person {
-        firstName: String
-        lastName: String
-        age: Int
-        fullName: String
-    }
-"""
-                )
+@graphQlArtifacts.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        # debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+# type_defs = gql("""
+#     type Query {
+#         kater: KaterQuery
+#     }
+#     type KaterQuery {
+#         genericView: Generic
+#     }
+#     scalar Generic
+# """)
+
+type_defs = load_schema_from_path('graphql_container')
 
 query = QueryType()
 
 
-@query.field("people")
+@query.field('kater')
+def resolve_kater(*_):
+    return {}
+
+
+katerQuery = ObjectType('KaterQuery')  # QueryType()
+
+
+@katerQuery.field("genericView")
 def resolve_people(*_):
     return [
         {"firstName": "Sushant", "lastName": "Agrawal", "age": 58},
         {"firstName": "Prashant", "lastName": "Agrawal", "age": 58}
     ]
-person = ObjectType("Person")
-@person.field("fullName")
-def resolve_person_full_name(person, *_):
-    return f"{person['firstName']} {person['lastName']}"
 
-schema = make_executable_schema(type_defs,query, person)
+schema = make_executable_schema(type_defs,katerQuery, query)  # person)
